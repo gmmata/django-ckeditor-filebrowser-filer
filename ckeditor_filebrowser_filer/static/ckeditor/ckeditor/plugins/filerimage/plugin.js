bbfb15c7a3f69f36df7745a415e14fb09cf1c6d8
anaelorlinski <ao@m9x.fr>
2022-07-07 17:16:15 +0200

    fix version url

--- ckeditor_filebrowser_filer/static/ckeditor/ckeditor/plugins/filerimage/plugin.js
+++ ckeditor_filebrowser_filer/static/ckeditor/ckeditor/plugins/filerimage/plugin.js
@@ -1,34 +1,34 @@
-(function(jQuery){
-    CKEDITOR.plugins.add( 'filerimage', {
+(function (jQuery) {
+    CKEDITOR.plugins.add('filerimage', {
         lang: 'en,it,fr,ru',
         icons: 'filerimage',
-        init: function( editor ) {
+        init: function (editor) {
             that = this,
-            lang = editor.lang.filerimage,
-            requiredContent = 'img[!src]',
-            allowedContent = 'img[!src,alt,width,height,align,filer_id,thumb_option,title,style]',
-            editor.addCommand( 'filerImageDialog', new CKEDITOR.dialogCommand( 'filerImageDialog',{
-                allowedContent: allowedContent,
-            } ) );
+                lang = editor.lang.filerimage,
+                requiredContent = 'img[!src]',
+                allowedContent = 'img[!src,alt,width,height,align,filer_id,thumb_option,title,style]',
+                editor.addCommand('filerImageDialog', new CKEDITOR.dialogCommand('filerImageDialog', {
+                    allowedContent: allowedContent,
+                }));
 
-            editor.ui.addButton( 'FilerImage', {
+            editor.ui.addButton('FilerImage', {
                 label: lang.name,
                 command: 'filerImageDialog',
                 toolbar: 'insert',
                 icon: 'filerimage',
             });
 
-            if ( editor.contextMenu ) {
-                editor.addMenuGroup( 'Filer' );
-                editor.addMenuItem( 'imageItem', {
+            if (editor.contextMenu) {
+                editor.addMenuGroup('Filer');
+                editor.addMenuItem('imageItem', {
                     label: lang.edit,
                     icon: this.path + 'icons/filerimage.png',
                     command: 'filerImageDialog',
                     group: 'Filer'
                 });
 
-                editor.contextMenu.addListener( function( element ) {
-                    if ( element.getAscendant( 'img', true ) ) {
+                editor.contextMenu.addListener(function (element) {
+                    if (element.getAscendant('img', true)) {
                         return { imageItem: CKEDITOR.TRISTATE_OFF };
                     }
                 });
@@ -36,29 +36,29 @@
 
             jQuery.get('/filebrowser_filer/url_reverse/',
                 { url_name: 'admin:index' },
-                function(data) {
+                function (data) {
                     editor.base_admin = data.replace(/\/$/, "");
-            });
+                });
 
-            jQuery.get('/filebrowser_filer/filer_version/', { }, function(data) {
+            jQuery.get('/filebrowser_filer/version/', {}, function (data) {
                 editor.filer_version = data;
-                if(data == '1.1' || data == '1.2') {
-                    CKEDITOR.scriptLoader.load( that.path + '../../../../filer/js/addons/popup_handling.js' );
+                if (data == '1.1' || data == '1.2') {
+                    CKEDITOR.scriptLoader.load(that.path + '../../../../filer/js/addons/popup_handling.js');
                 }
-                else if(data == '1.0') {
-                    CKEDITOR.scriptLoader.load( that.path + '../../../../filer/js/popup_handling.js' );
+                else if (data == '1.0') {
+                    CKEDITOR.scriptLoader.load(that.path + '../../../../filer/js/popup_handling.js');
                 }
-                CKEDITOR.dialog.add( 'filerImageDialog', that.path + 'dialogs/filerImageDialog.js' );
+                CKEDITOR.dialog.add('filerImageDialog', that.path + 'dialogs/filerImageDialog.js');
             });
 
-            jQuery.get('/filebrowser_filer/setting/use_thumbnailoptions_only/', { }, function(data) {
+            jQuery.get('/filebrowser_filer/setting/use_thumbnailoptions_only/', {}, function (data) {
                 editor.use_thumbnailoptions_only = data
             });
-            jQuery.get('/filebrowser_filer/setting/use_canonical_for_thumbnails/', { }, function(data) {
-                if(data == '0') {
+            jQuery.get('/filebrowser_filer/setting/use_canonical_for_thumbnails/', {}, function (data) {
+                if (data == '0') {
                     editor.use_canonical_for_thumbnails = false;
                 }
-                else if(data == '1') {
+                else if (data == '1') {
                     editor.use_canonical_for_thumbnails = true;
                 }
                 else {
@@ -68,11 +68,11 @@
 
             jQuery.get('/filebrowser_filer/url_reverse/',
                 { url_name: 'admin:filer-directory_listing-last' },
-                function(data) {
+                function (data) {
 
-            });
+                });
 
             var dialog = CKEDITOR.dialog.getCurrent();
         }
     });
-})($||django.jQuery||jQuery);
+})($ || django.jQuery || jQuery);
